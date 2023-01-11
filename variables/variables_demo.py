3a0bb3e49e2940aa808faf99645f26b24f883919
Anna Kalacheva <annakalacheva@Annas-MacBook-Air.local>
2023-01-10 17:27:35 -0800

    Init commit

---
+++ variables/variables_demo.py
@@ -0,0 +1,19 @@
+
+
+name = "Anna"
+title = "Sr. Software Engineer in Test"
+field = "Software Engineering"
+age = 33
+
+
+print('========')
+
+print("Current user's name:" + name)
+print("Current user's title:" + title)
+print("Current user's age:" + str(age))
+
+print("")
+
+
+
+
